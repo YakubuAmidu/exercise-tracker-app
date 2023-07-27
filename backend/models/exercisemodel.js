7fdc73f2954c082f2bd8d6b82e3e0d4b278ab03d
Yakubuamidu <yakubuamidu577@gmail.com>
2023-07-27 10:35:57 -0700

    Initial commit

--- backend/models/exercisemodel.js
+++ backend/models/exercisemodel.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 
+const Schema = mongoose.Schema;
+
 const exerciseSchema = new Schema({
     username: { type: String, required: true },
     description: { type: String, required: true },
