8d865515ad97d3ee13126e5e1e5fa1658f67e696
Yakubuamidu <yakubuamidu577@gmail.com>
2023-07-27 10:21:01 -0700

    Initial commit

---
+++ backend/models/usermodel.js
@@ -0,0 +1,16 @@
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        minlength: 3
+    }
+},
+{
+    timestamp: true
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
