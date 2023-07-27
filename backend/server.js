ba0d5f7e113d3a11e5a4b8758cad55a61b161922
Yakubuamidu <yakubuamidu577@gmail.com>
2023-07-27 10:07:54 -0700

    Initial commit

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8484;
 
 app.use(cors());
 app.use(express.json());
@@ -14,7 +14,7 @@ mongoose.connect(process.env.ATLAS_URI)
         .then(() => {
             // Listen for request
             app.listen(port, () => {
-              console.log(`Connected to mongodb, and listening on port ${port}`);
+              console.log(`Connected to mongodb database, and listening on port ${port}`);
             });
         }).catch((err) => {
             console.log('Error: ' + err.message);
