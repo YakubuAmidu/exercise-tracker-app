5f86c51281b5e3db84bde4ed35461147742191db
Yakubuamidu <yakubuamidu577@gmail.com>
2023-08-01 09:32:14 -0700

    Initial commit

--- backend/routes/exercises.js
+++ backend/routes/exercises.js
@@ -33,5 +33,11 @@ router.route('/:id').get((req, res) => {
                 .catch((err) => res.status(400).json('Error: ' + err.message));
 });
 
+router.route('/:id').delete((req, res) => {
+    Exercise.findByIdAndDelete(req.params.id)
+            .then((exercise) => res.json(exercise))
+            .catch((err) => res.status(400).json("Error: " + err.message));
+});
+
 module.exports = router;
 
