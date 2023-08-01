380786be726535a284811410bb2fc05f901c71e4
Yakubuamidu <yakubuamidu577@gmail.com>
2023-08-01 10:49:54 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="container">
-      Hello world!
-    </div>
+    <Router>
+      <Navbar />
+      <Route path='/' exact Component={ExercisesList} />
+      <Route path='/edit/:id' exact Component={EditExercise} />
+      <Route path='/create' Component={CreateExercise} />
+      <Route path='/user' Component={CreateUser} />
+    </Router>
   );
 }
 
