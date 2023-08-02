877efbedf801c7565c6e161c8d8de35a4d65a8dc
Yakubuamidu <yakubuamidu577@gmail.com>
2023-08-02 10:52:42 -0700

    Initial commit

--- src/components/create-exercise.component.js
+++ src/components/create-exercise.component.js
@@ -72,13 +72,17 @@ export default class CreateExercise extends Component {
             <form onSubmit={this.onSubmit}>
               <div className="form-group">
                 <label>Username: </label>
-                <select ref="userInput" 
-                required className='form-control'
+                <select 
+                ref="userInput" 
+                required 
+                className='form-control'
                 value={this.state.username}
                 onChange={this.onChangeUsername}>
                     {
                         this.state.users.map(function(user){
-                          return <option key={user} value={user}>
+                          return <option 
+                          key={user} 
+                          value={user}>
                               {
                                 user
                               }
@@ -86,6 +90,20 @@ export default class CreateExercise extends Component {
                         })
                     }
                 </select>
+              </div>
+               
+              <div className="form-group">
+                <label>Description: </label>
+                <input 
+                type="text" 
+                required className='form-control' 
+                value={this.state.description}
+                onChange={this.onChangeDescription}
+                />
+              </div>
+
+              <div className="form-group">
+                
               </div>
             </form>
         </div>
