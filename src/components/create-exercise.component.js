b7b3a804c74f39158ca6e2807ec2b409d7a56503
Yakubuamidu <yakubuamidu577@gmail.com>
2023-08-02 10:10:47 -0700

    Initial commit

--- src/components/create-exercise.component.js
+++ src/components/create-exercise.component.js
@@ -12,7 +12,31 @@ export default class CreateExercise extends Component {
             users: [],
         }
     }
-    
+
+    onChangeUsername(e){
+        this.setState({
+            username: e.target.value
+        })
+    }
+
+    onChangeDescription(e){
+        this.setState({
+            description: e.target.value
+        })
+    }
+
+    onChangeDuration(e){
+        this.setState({
+            duration: e.target.value
+        })
+    }
+
+    onChangeDate(date){
+        this.setState({
+            date: date
+        })
+    }
+
     render(){
         return (
         <div>
