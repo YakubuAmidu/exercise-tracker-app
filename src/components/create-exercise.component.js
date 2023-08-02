0e93558164891b07eb733a9153ab9dabcc4b8ab1
Yakubuamidu <yakubuamidu577@gmail.com>
2023-08-02 10:19:26 -0700

    Initial commit

--- src/components/create-exercise.component.js
+++ src/components/create-exercise.component.js
@@ -4,6 +4,11 @@ export default class CreateExercise extends Component {
     constructor(props){
         super(props);
 
+        this.onChangeUsername = this.onChangeUsername.bind(this);
+        this.onChangeDescription = this.onChangeDescription.bind(this);
+        this.onChangeDuration = this.onChangeDuration.bind(this);
+        this.onChangeDate = this.onChangeDate.bind(this);
+
         this.state = {
             username: '',
             description: '',
@@ -48,6 +53,8 @@ export default class CreateExercise extends Component {
         }
 
         console.log('Exercise: ' + exercise);
+
+        window.location = '/';
     }
 
     render(){
