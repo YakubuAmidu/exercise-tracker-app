56d1c841ca8230e3b3fb26f97f474c81895fd6c8
Yakubuamidu <yakubuamidu577@gmail.com>
2023-08-04 08:12:32 -0700

    Initial commit

--- src/components/exercise-list.component.js
+++ src/components/exercise-list.component.js
@@ -4,6 +4,23 @@ import { Link } from 'react-router-dom';
 
 import axios from 'axios';
 
+const Exercise = props => (
+    <tr>
+        <td>{props.exercise.username}</td>
+        <td>{props.exercise.description}</td>
+        <td>{props.exercise.duration}</td>
+        <td>{props.exercise.date.substring(0, 10)}</td>
+        <td>
+            <Link to={'/edit/' + props.exercise._id}>Edit</Link> 
+            | 
+            <a href="https://www.google.com" 
+            onClick={() => { props.deleteExercise(props.exercise._id)}}
+            >Delete
+            </a>
+        </td>
+    </tr>
+)
+
 export default class ExerciseList extends Component {
     constructor(props){
         super(props);
@@ -17,9 +34,8 @@ export default class ExerciseList extends Component {
         axios.get('http://localhost:5000/exercises/')
               .then((response) => {
                 this.setState({ exercises: response.data })
-                .catch((error) => {
+                }).catch((error) => {
                     console.log(error.message);
-                })
               })
     }
 
@@ -29,10 +45,32 @@ export default class ExerciseList extends Component {
                 this.setState({ exercises: this.state.exercises.filter(el => el._id !== id )});
     }
 
+    exerciseList(){
+        return this.state.exercises.map((currentExercise) => {
+            return <Exercise exercise={currentExercise} deleteExercise={this.deleteExercise} key={currentExercise._id} />
+        })
+    }
+
     render(){
         return (
             <div>
-                <p>You are on the exercise list component</p>
+                <h3>Logged Exercises</h3>
+                <table className="table">
+                    <thead className="thead-light">
+                        <tr>
+                            <th>Username</th>
+                            <th>Description</th>
+                            <th>Duration</th>
+                            <th>Date</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            this.exerciseList()
+                        }
+                    </tbody>
+                </table>
             </div>
         )
     }
