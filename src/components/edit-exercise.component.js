ca9441491f04cbbc3e2047663a794fa90fa66c54
Yakubuamidu <yakubuamidu577@gmail.com>
2023-08-20 11:12:06 -0700

    Initial commit

--- src/components/edit-exercise.component.js
+++ src/components/edit-exercise.component.js
@@ -20,9 +20,73 @@ export default class EditExercise extends Component {
             date: new Date(),
             users: []
         }
-
     }
-    
+
+       componentDidMount(){
+            axios.get('http://localhost:5000/exercises/' + this.props.match.params.id).then((response) => {
+                this.setState({
+                    username: response.data.username,
+                    description: response.data.description,
+                    duration: response.data.duration,
+                    date: new Date(response.data.date)
+                }).catch(error => {
+                    console.log('Error: ' + error.message);
+                });
+            });
+
+            axios.get('http://localhost:5000/users/')
+                 .then((response) => {
+                    if(response.data.length > 0){
+                        this.setState({
+                            users: response.data.map(user => user.username)
+                        })
+                    }
+                 })
+        }
+
+        onChangeUsername(e){
+            this.setState({
+              username: e.target.value
+            });
+        }
+
+        onChangeDescription(e){
+           this.setState({
+             description: e.target.value
+           });
+        }
+
+        onChangeDuration(e){
+            this.setState({
+                duration: e.target.value
+            });
+        }
+
+        onChangeDate(date){
+            this.setState({
+                date: date
+            });
+        }
+
+        onSubmit(e){
+           e.preventDefault();
+
+           const exercise = {
+               username: this.state.username,
+               description: this.state.description,
+               duration: this.state.duration
+           }
+
+           console.log('Exercise: ' + exercise);
+
+           axios.post('http://localhost:5000/exercises/update' + this.props.match.params.id, exercise)
+                .then((res) => {
+                    console.log(res.data);
+
+                    window.location = '/';
+                })
+        }
+
     render(){
         return (
             <div>
